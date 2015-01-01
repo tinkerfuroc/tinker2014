55278578be03c73b4b79df7c2d493833cf94c779
bssthu <bssda03@qq.com>
2015-01-01 17:44:50 +0800

    add missing downloaded workspace

--- tinkersetup.bash
+++ tinkersetup.bash
@@ -1,9 +1,10 @@
 # tinkersetup.bash
 # created by bss at 2014-12-28
-# Last modified: 2014-12-28, 17:09:24
+# Last modified: 2015-01-01, 17:44:26
 
 source $TINKER_WORKSPACE/dev/devel/setup.bash
 source $TINKER_WORKSPACE/driver/devel/setup.bash --extend
 source $TINKER_WORKSPACE/logic/devel/setup.bash --extend
 source $TINKER_WORKSPACE/decision/devel/setup.bash --extend
+source $TINKER_WORKSPACE/downloaded/devel/setup.bash --extend
 
