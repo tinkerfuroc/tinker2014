c79ac14ece6909ad0de816aef16e8bf8ad2e4dc8
bssthu <bssda03@qq.com>
2015-01-01 17:18:20 +0800

    add scripts for l_sphinx_wrapper

---
+++ ui/l_sphinx_wrapper/README.md
@@ -0,0 +1,66 @@
+##l_sphinx_wrapper的附加脚本
+
+
+###init.py
+
+初始化一个任务文件夹
+
+####使用方法
+
+```bash
+./init.py --init TASK_NAME
+```
+
+这条命令会创建$TINKER_WORKSPACE/share/l_sphinx_wrapper/launches/TASK_NAME/，
+其中有文件 sent.txt, gram.jsgf。
+
+sent.txt 和 gram.jsgf 的编辑方法：
+- 编辑sent.txt，写入可能出现的词语，如
+  ```
+  hello tinker
+  my name is
+  alex
+  bob
+  ```
+
+- 然后编辑gram.jsgf，写入语法，注意单词必须是大写，如
+  ```
+  #JSGF v1.0;
+  grammar furoc;
+  public <furocCmd> = <myname> | <hellotinker>;
+  <myname> = MY NAME IS <names>;
+  <names> = TOM | BOB | JACK;
+  <hellotinker> = HELLO <names>;
+  ```
+
+编辑完成后，生成 .dict, .fsg, .launch 文件：
+
+```bash
+./init.py -t TASK_NAME
+```
+
+这条命令等价于
+
+```bash
+python input2dict.py -t TASK_NAME
+python jsgf2fsg.py -t TASK_NAME
+```
+
+之后便可以正常使用：
+
+```bash
+roslaunch l_sphinx_wrapper TASK_NAME.launch
+```
+
+------
+
+###toUpper.py
+
+将文件(例如gram.jsgf)中的字母转换为大写
+
+####使用方法
+
+```bash
+python toUpper.py -i ../../share/l_sphinx_wrapper/answer_questions/gram.jsgf
+```
+
---
+++ ui/l_sphinx_wrapper/init.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# file: init.py
+# created by bss at 2014-05-09
+# Last modified: 2015-01-01, 17:17:09
+# 初始化一组语音识别的任务
+#
+
+import sys
+import os
+import getopt
+import rospkg
+
+def Usage():
+    print('init.py usage:')
+    print('初始化一组语音识别任务，处理${task}/sent.txt,gram.jsgf')
+    print('-h,--help: print help message.')
+    print('--init: init a dir for your task')
+    print('-t,--task: input the name of the task.')
+    print('')
+    print('example:')
+    print('python init.py --init whoiswho')
+    print('python init.py -t whoiswho')
+
+def main(argv):
+    if len(argv) <= 1:
+        Usage()
+        sys.exit(2)
+    try:
+        opts, args = getopt.getopt(argv[1:], 'ht:',
+                ['help', 'task=', 'init='])
+    except getopt.GetoptError, err:
+        print(str(err))
+        Usage()
+        sys.exit(2)
+    except:
+        Usage()
+        sys.exit(1)
+
+    for o, a in opts:
+        if o in ('-h', '--help'):
+            Usage()
+            sys.exit(0)
+        elif o in ('--init'):
+            initDir(a)
+            sys.exit(0)
+        elif o in ('-t', '--task'):
+            processTask(a)
+            sys.exit(0)
+
+def initDir(task):
+    scriptdir = rospkg.RosPack().get_path('l_sphinx_wrapper') \
+            + '/../../../ui/l_sphinx_wrapper'
+    taskdir = scriptdir + '/' + task
+    os.system('mkdir ' + taskdir)
+    os.system('touch ' + taskdir + '/sent.txt')
+    os.system('touch ' + taskdir + '/gram.jsgf')
+
+    fp = open(taskdir + task + '.launch', 'w')
+    fp.write('<launch>\n')
+    fp.write('\n')
+    fp.write('  <node name="recognizer" pkg="pocketsphinx" ' \
+            + 'type="recognizer.py" output="screen">\n')
+    fp.write('    <param name="fsg" ' \
+            + 'value="$(find l_sphinx_wrapper)/launches/' \
+            + task + '/finite_state.fsg"/>\n')
+    fp.write('    <param name="dict" ' \
+            + 'value="$(find l_sphinx_wrapper)/launches/'
+            + task + '/words.dic"/>\n')
+    fp.write('  </node>\n')
+    fp.write('\n')
+    fp.write('</launch>\n')
+    fp.close()
+
+def processTask(task):
+    scriptdir = rospkg.RosPack().get_path('l_sphinx_wrapper') \
+            + '/../../../ui/l_sphinx_wrapper'
+    taskdir = scriptdir + '/' + task
+    os.system('python ' + scriptdir + '/input2dict.py -t ' + task)
+    os.system('python ' + scriptdir + '/jsgf2fsg.py -t ' + task)
+    
+
+if __name__ == '__main__':
+    main(sys.argv)
