ea82c70cdba8d7fbe9c11bf00f59ffa771efcc78
bssthu <bssda03@gmail.com>
2015-01-28 21:43:42 +0800

    migrate pcl driver

---
+++ driver/src/d_pcl/src/reading_pointcloud.cpp
@@ -0,0 +1,38 @@
+// Project      : reading_pointcloud
+// File         : reading_pointcloud.cpp
+// Author       : bss
+// Creation Date: 2014-07-12
+// Last modified: 2015-01-28, 21:40:59
+// Description  : convert ros-style pointcloud to pcl-style.
+// 
+
+#include <stdio.h>
+#include <string>
+#include <ros/ros.h>
+#include <d_pcl/reading_pointcloud.h>
+
+CloudConverter::CloudConverter()
+    : nh_()
+    , ready_xyz_(false)
+    , ready_xyzrgb_(false)
+{
+    sub_xyz_ = nh_.subscribe<pcl::PointCloud<pcl::PointXYZ> >("/openni/points2_xyz", 1, &CloudConverter::cloudXYZCb, this);
+    sub_xyzrgb_ = nh_.subscribe<pcl::PointCloud<pcl::PointXYZRGB> >("/openni/points2", 1, &CloudConverter::cloudXYZRGBCb, this);
+}
+
+CloudConverter::~CloudConverter()
+{
+}
+    
+void CloudConverter::cloudXYZCb(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& msg)
+{
+    msg_xyz_ = msg;
+    ready_xyz_ = true;
+}
+
+void CloudConverter::cloudXYZRGBCb(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& msg)
+{
+    msg_xyzrgb_ = msg;
+    ready_xyzrgb_ = true;
+}
+
---
+++ driver/src/d_pcl/test/sub_xyzrgb.cpp
@@ -0,0 +1,44 @@
+// Project      : reading_pointcloud
+// File         : sub_xyz.cpp
+// Author       : bss
+// Creation Date: 2014-07-12
+// Last modified: 2015-01-28, 21:41:44
+// Description  : convert ros-style pointcloud to pcl-style.
+// 
+
+#include <stdio.h>
+#include <string>
+#include <ros/ros.h>
+#include <d_pcl/reading_pointcloud.h>
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "test_sub_cloud_xyzrgb");
+    ros::NodeHandle n;
+    ros::Rate rate(33);
+
+    CloudConverter* cc_ = new CloudConverter();
+
+    while (!cc_->ready_xyzrgb_)
+    {
+        ros::spinOnce();
+        rate.sleep();
+        if (!ros::ok())
+        {
+            printf("Terminated by C-c.\n");
+            return -1;
+        }
+    }
+
+    while (ros::ok())
+    {
+        pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr msg = cc_->msg_xyzrgb_;
+        printf ("%s, Cloud: width = %d, height = %d\n", msg->header.frame_id.c_str(), msg->width, msg->height);
+        rate.sleep();
+    }
+
+    delete cc_;
+    cc_ = NULL;
+
+    return 0;
+}
